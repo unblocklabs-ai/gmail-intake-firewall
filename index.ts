25ec2526e1779d42ba3eef388d502e87085c39f0
Bek <bek.akhmedov@gmail.com>
2026-05-06 16:33:51 -0400

    release: v0.1.6

--- index.ts
+++ index.ts
@@ -3,7 +3,7 @@ import { registerGmailIntakeFirewallPlugin } from "./src/plugin.js";
 export default {
   id: "gmail-intake-firewall",
   name: "Gmail Intake Firewall",
-  version: "0.1.5",
+  version: "0.1.6",
   register(api: unknown) {
     registerGmailIntakeFirewallPlugin(api);
   },
--- marketplace/gmail-intake-firewall/dist/index.js
+++ marketplace/gmail-intake-firewall/dist/index.js
@@ -2,7 +2,7 @@ import { registerGmailIntakeFirewallPlugin } from "./src/plugin.js";
 export default {
     id: "gmail-intake-firewall",
     name: "Gmail Intake Firewall",
-    version: "0.1.5",
+    version: "0.1.6",
     register(api) {
         registerGmailIntakeFirewallPlugin(api);
     },
